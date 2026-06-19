739e5c0650900800932a996fb156a7453105fe27
Haikmet <smbayirli@gmail.com>
2026-06-18 20:19:46 -0700

    Tried setting up rep

---
+++ src/main/java/net/haikmet/emeraldset/EmeraldSetsDataGenerator.java
@@ -0,0 +1,11 @@
+package net.haikmet.emeraldset;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class EmeraldSetsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
